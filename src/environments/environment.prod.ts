74e3db22e76b6028a46baf3ba09116436d94b9d4
Anacleto Junior <126117483+anacletojunior@users.noreply.github.com>
2025-04-11 20:48:30 -0300

    Update environment.prod.ts
    remove ev prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,1 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.jsonbin.io/v3/b/67f898a38960c979a582cbbd',
-  apiKey: '$2a$10$vgCdwcQFiMBCYKAU.MjSIuJCJgovINPU.cbtKW/IXJ0i3pMzn7ALG'
-};
+
