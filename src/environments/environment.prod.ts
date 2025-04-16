520162d379b38484298167745b458868cf38ab0e
Anacleto Junior <anacleto.junior@outlook.com>
2025-04-16 20:46:30 -0300

    api on render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.jsonbin.io/v3/b/67f898a38960c979a582cbbd',
+  apiUrl: 'https://quizz-api-8ae1.onrender.com/quizz',
   apiKey: '$2a$10$vgCdwcQFiMBCYKAU.MjSIuJCJgovINPU.cbtKW/IXJ0i3pMzn7ALG'
 };
